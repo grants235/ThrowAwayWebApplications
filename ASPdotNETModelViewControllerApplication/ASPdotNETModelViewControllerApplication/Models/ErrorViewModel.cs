56236a94847b3ae65ab0a0fbd39a74f55d572068
Grant <grantshanklin@gmail.com>
2020-04-30 16:13:38 -0500

    InitialCommit

---
+++ ASPdotNETModelViewControllerApplication/ASPdotNETModelViewControllerApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASPdotNETModelViewControllerApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
