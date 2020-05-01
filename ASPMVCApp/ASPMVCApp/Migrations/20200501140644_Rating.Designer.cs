ea4549a3c41bf6ff31f84ce1c3db90acce8e1f91
Grant <grantshanklin@gmail.com>
2020-05-01 09:07:22 -0500

    Added Rating Field

--- ASPMVCApp/ASPMVCApp/Migrations/20200501140644_Rating.Designer.cs
+++ ASPMVCApp/ASPMVCApp/Migrations/20200501140644_Rating.Designer.cs
@@ -4,14 +4,16 @@ using ASPMVCApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ASPMVCApp.Migrations
 {
     [DbContext(typeof(MVCMovieContext))]
-    partial class MVCMovieContextModelSnapshot : ModelSnapshot
+    [Migration("20200501140644_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
