7babb86b3a6087bc778a61926a90a41d3db6b175
Grant <grantshanklin@gmail.com>
2020-05-02 15:08:15 -0500

    Added login

---
+++ ASPIdentityMVC/ASPIdentityMVC/Controllers/AuthenticateController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPIdentityMVC.Controllers
+{
+    public class AuthenticateController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult CheckUser(string username, string password)
+        {
+
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
