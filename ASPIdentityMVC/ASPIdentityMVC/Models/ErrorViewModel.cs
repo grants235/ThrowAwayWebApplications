8a111c3af64e641b8590879604c3f95e1fe936fb
Grant <grantshanklin@gmail.com>
2020-05-02 14:48:04 -0500

    Added new sloution

---
+++ ASPIdentityMVC/ASPIdentityMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASPIdentityMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
