37767365799a2d986cea3c13fb3cd9cd99d259bf
Grant <grantshanklin@gmail.com>
2020-06-08 09:22:14 -0500

    Added new project

---
+++ ASPIdentityMVCApp/ASPIdentityMVCApp/Migrations/202005022220194_InitialCreate.cs
@@ -0,0 +1,28 @@
+namespace ASPIdentityMVCApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 50),
+                        Password = c.String(),
+                        ConfirmPassword = c.String(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+        }
+    }
+}
---
+++ ASPIdentityMVCApp/ASPIdentityMVCApp/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ASPIdentityMVCApp.Models
+{
+    public class User
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Enter Username")]
+        [StringLength(50)]
+        [MinLength(2, ErrorMessage = "Username is too short")]
+        [ScaffoldColumn(true)]
+        public string Name { get; set; }
+
+        [Display(Name = "Enter password")]
+        [MinLength(5, ErrorMessage = "Password is too short")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm your password")]
+        [Compare("password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
